85f65ce9c295524e2afbb1fc9129804b5b9cc6ae
isma <vieja069gmail.com>
2025-04-09 15:11:23 -0300

    se agrego un ejemplo de psint

--- js/1-variables.js
+++ js/1-variables.js
@@ -4,8 +4,18 @@ document.writeln('<p class="parrafoColor">hola nuevamente</p>')
 document.writeln('<a href="http://127.0.0.1:5500/">live server</a>')
  
 let YearActual
+let precio = 2.50
 
 YearActual = 2025
 document.writeln('<br>el año actual es: ',YearActual)
 YearActual = 2024
 document.writeln('<br>el contenido de YearActual es: ',YearActual)
+
+const url = "http://127.0.0.1:5500/";
+document.writeln('<br>la direccion de live server: ',url) 
+
+let numero1,numero2,resultado;
+numero1 = 12;
+numero2 = 2;
+resultado = numero1+numero2;
+document.writeln('<br>el resultado es: ',resultado) 
