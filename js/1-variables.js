a2c980c7aa3543b03284a2878b9fdebc2370ecba
isma <vieja069gmail.com>
2025-04-09 14:49:14 -0300

    variables y contenido

---
+++ js/1-variables.js
@@ -0,0 +1,11 @@
+console.log("HOLA MEEEN")
+
+document.writeln('<p class="parrafoColor">hola nuevamente</p>')
+document.writeln('<a href="http://127.0.0.1:5500/">live server</a>')
+ 
+let YearActual
+
+YearActual = 2025
+document.writeln('<br>el año actual es: ',YearActual)
+YearActual = 2024
+document.writeln('<br>el contenido de YearActual es: ',YearActual)
