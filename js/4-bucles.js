3b4bf57acf1b3e5461ecb3c274617f44bbe4199f
isma <vieja069gmail.com>
2025-04-18 18:07:13 -0300

    se agrego un ejemplo de bucle do-while

--- js/4-bucles.js
+++ js/4-bucles.js
@@ -4,3 +4,10 @@ while (renglon <= 50){
     document.writeln(`<p>Hola, soy un renglon ${renglon}</p>`);
     renglon++; 
 }
+
+let linea = 1;
+
+do{
+    document.writeln(`<p>Hola, soy un renglon ${linea}</p>`);
+    linea++;
+} while (linea <= 10);
