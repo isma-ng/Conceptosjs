bd4779f92b82a35ee85655cf5202660a4807111a
isma <vieja069gmail.com>
2025-04-18 16:06:31 -0300

    no se que paso

---
+++ js/3-switch.js
@@ -0,0 +1,27 @@
+const opcions =  parseInt(prompt('selecccione una opcion: \n1. consultar saldo\n2. depositar dinero\n3. extraer dinero\n4. salir'));
+ 
+let saldo = 10000;
+switch (opcions) {
+    case 1:
+        document.writeln(`su saldo es de ${saldo}`);
+        break;
+    case 2:
+        const depositar = Number(prompt('cuanto dinero desea depositar?'));
+        saldo += depositar;
+        document.writeln(`depositaste $ ${depositar}, su saldo es de $ ${saldo}`);
+        break;
+        case 3:
+        const extraer = Number(prompt('cuanto dinero desea extraer?'));
+        if(extraer <= saldo){
+        saldo  -= extraer;
+        document.writeln(`extrajiste $-${extraer}, su saldo es de $ ${saldo}`);
+        } else{
+            document.writeln(`saldo insuficiente, su saldo es de $ ${saldo}`);
+        }  
+        break;
+        case 4:
+        document.writeln('saliste de la app');  
+        break;
+        default:
+        document.writeln('opcion no valida, por favor seleccione una opcion valida');
+}
