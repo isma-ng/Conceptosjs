8b14d18ef0819c60cb8550e8636cce31cff20c38
isma <vieja069gmail.com>
2025-04-24 22:38:04 -0300

    se agrego ejemplos de funciones

---
+++ js/6-funciones.js
@@ -0,0 +1,33 @@
+//Sintaxis: funcion tradicinal, expresion de funcion, arrow functions
+
+//funciones sin parametros
+function saludar(){
+    //todas las lineas de codigo que quiero hacer para saludar
+    document.writeln('<p>Hola mundo 🌎</p>')
+}
+
+//funciones con parametros
+function sumar(numero1,numero2){
+    const resultado = numero1 + numero2
+    document.writeln(`<p>Resultado de la suma: ${resultado}</p>`)
+}
+
+//funciones que retornan un valor
+function multiplicar(numero1, numero2){
+    const resultado = numero1 * numero2
+    console.log(resultado)
+    return resultado
+}
+
+//invocar o llamar a una funcion
+saludar();
+
+const num1 = parseInt(prompt('ingresa un numero'))
+const num2 = parseInt(prompt('ingresa un segundo'))
+
+sumar(num1,num2);
+sumar(num1, 10)
+
+// const resultado = multiplicar(num1, num2)
+// document.writeln('El resultado de la multiplicacion es:'+ resultado)
+document.writeln('El resultado de la multiplicacion es:'+ multiplicar(num1, num2))
